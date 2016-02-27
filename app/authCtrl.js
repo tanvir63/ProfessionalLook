1bf52489204affab8d9f1bb3fcd09d1b33e84c44
tanvir63 <tanvir63@ymail.com>
2016-02-26 19:41:55 -0500

    Admin panel

---
+++ app/authCtrl.js
@@ -0,0 +1,20 @@
+app.controller('authCtrl', function ($scope, $rootScope, $routeParams, $location, $http, Data) {
+    //initially set those objects to null to avoid undefined error
+    $scope.login = {};
+    $scope.doLogin = function (customer) {
+        Data.post('login', {
+            customer: customer
+        }).then(function (results) {
+            Data.toast(results);
+            if (results.status == "success") {
+                $location.path('dashboard');
+            }
+        });
+    };
+    $scope.logout = function () {
+        Data.get('logout').then(function (results) {
+            Data.toast(results);
+            $location.path('login');
+        });
+    }
+});
